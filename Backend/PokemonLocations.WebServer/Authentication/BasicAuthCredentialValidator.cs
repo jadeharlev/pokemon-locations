c3a1013c0e7a863d14a011c93b4f79d58d681465
Jade Harlev <8244534+jadeharlev@users.noreply.github.com>
2026-04-28 16:27:55 -0700

    Remove hardcoded "Basic" string

--- Backend/PokemonLocations.WebServer/Authentication/BasicAuthCredentialValidator.cs
+++ Backend/PokemonLocations.WebServer/Authentication/BasicAuthCredentialValidator.cs
@@ -1,4 +1,5 @@
 using System.Security.Claims;
+using idunno.Authentication.Basic;
 using PokemonLocations.WebServer.Database.Repositories;
 
 namespace PokemonLocations.WebServer.Authentication;
@@ -23,7 +24,7 @@ public class BasicAuthCredentialValidator {
                 new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
                 new Claim(ClaimTypes.Name, user.DisplayName)
             ],
-            "Basic");
+            BasicAuthenticationDefaults.AuthenticationScheme);
 
         return new ValidationResult(true, new ClaimsPrincipal(identity));
     }
